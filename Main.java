451d1503bd6197f1c1349a2d2288f53e4a800747
HemnNariman <learning1125r@gmail.com>
2025-09-02 14:16:58 +0300

    Add Movie polymorphism project

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner watchMovie = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter Type (A for Adventure, C for Comedy, " + "S for Science Fiction, or Q to quit): ");
+            String type = watchMovie.nextLine();
+            if (type.equalsIgnoreCase("Q")) {
+                break;
+            }
+            if (type.isEmpty()) {
+                continue;
+            }
+            System.out.println("Enter Movie Title: ");
+            String title = watchMovie.nextLine();
+
+
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
+
+        watchMovie.close();
+    }
+}
